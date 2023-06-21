fb82d698f2701bcfa3ba1a1fa8112cf2d618fb96
SushankSinha <116825226+SushankSinha@users.noreply.github.com>
2023-06-22 02:21:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading";
+import Items from "./Items";
+
+function App(){
+  return(
+    <div>
+    <Heading />
+    <Items />
+  </div>
+  )
+}
+
+export default App
